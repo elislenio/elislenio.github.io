fe5e8317a7347574059de6e9264cc99c0ff601ec
Guido Donnari <elislenio@users.noreply.github.com>
2015-08-29 21:45:12 -0300

    Update basic-ss.js

--- examples/basic-ss.js
+++ examples/basic-ss.js
@@ -3,7 +3,7 @@ flExamples.controller('ssBasicPhonesCtrl', ['$scope', function($scope) {
 	//Set up the FL options
 	$scope.phones2 = {};
 	$scope.phones2.options = {
-		listURL: '/examples/db/get_phones.php',
+		listURL: 'http://flexilist.16mb.com/examples/db/get_phones.php',
 		method: 'GET',
 		urlencoded: true,
 		limit: 4,
