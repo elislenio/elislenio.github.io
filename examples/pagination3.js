5b12743d4d8fe543ee1d0dd939ed190c4bd55694
Guido Donnari <elislenio@users.noreply.github.com>
2015-08-29 22:04:01 -0300

    Update pagination3.js

--- examples/pagination3.js
+++ examples/pagination3.js
@@ -2,7 +2,7 @@ flExamples.controller('paginationPhones3ExCtrl', ['$scope', function($scope) {
 	//Set up FL options
 	$scope.phones3 = {};
 	$scope.phones3.options = {
-		listURL: '/examples/db/get_phones.php', 
+		listURL: 'http://flexilist.16mb.com/examples/db/get_phones.php', 
         method: 'GET', 
         urlencoded: true, 
 		pagesize: 5,
