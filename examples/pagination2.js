a6e77941253cde06848f261d994274ec6c6a73cd
Guido Donnari <elislenio@users.noreply.github.com>
2015-08-29 22:04:19 -0300

    Update pagination2.js

--- examples/pagination2.js
+++ examples/pagination2.js
@@ -2,7 +2,7 @@ flExamples.controller('paginationPhones2ExCtrl', ['$scope', function($scope) {
 	//Set up FL options
 	$scope.phones2 = {};
 	$scope.phones2.options = {
-		listURL: '/examples/db/get_phones.php', 
+		listURL: 'http://flexilist.16mb.com/examples/db/get_phones.php', 
         method: 'GET', 
         urlencoded: true, 
 		pagesize: 5,
