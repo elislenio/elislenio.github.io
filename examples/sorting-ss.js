bc2d840b2add0b9815528b36fd16c251e4587b3d
Guido Donnari <elislenio@users.noreply.github.com>
2015-08-29 22:00:51 -0300

    Update sorting-ss.js

--- examples/sorting-ss.js
+++ examples/sorting-ss.js
@@ -3,7 +3,7 @@ flExamples.controller('sortPhonesServerCtrl', ['$scope', function($scope) {
 	//Set up the FL options with a default sorting on server side
 	$scope.phones2 = {};
 	$scope.phones2.options = {
-		listURL: '/examples/db/get_phones.php',
+		listURL: 'http://flexilist.16mb.com/examples/db/get_phones.php',
 		method: 'GET',
 		urlencoded: true,
 		orderby: [
