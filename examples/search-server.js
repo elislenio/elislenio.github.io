b7350f0798b8185be088588a94cab24634b90440
Guido Donnari <elislenio@users.noreply.github.com>
2015-08-29 22:01:58 -0300

    Update search-server.js

--- examples/search-server.js
+++ examples/search-server.js
@@ -41,7 +41,7 @@ flExamples.controller('searchPhonesServerCtrl', ['$scope', function($scope) {
 	$scope.phones2.search_value = 'dell';
 	
 	$scope.phones2.options = {
-		listURL: '/examples/db/get_phones.php',
+		listURL: 'http://flexilist.16mb.com/examples/db/get_phones.php',
 		method: 'GET',
 		urlencoded: true,
 		limit: 5,
