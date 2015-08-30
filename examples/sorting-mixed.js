ab75bae2f8e14923782334446be381b973e32a80
Guido Donnari <elislenio@users.noreply.github.com>
2015-08-29 22:00:26 -0300

    Update sorting-mixed.js

--- examples/sorting-mixed.js
+++ examples/sorting-mixed.js
@@ -3,7 +3,7 @@ flExamples.controller('sortPhonesMixedCtrl', ['$scope', function($scope) {
 	//Set up the FL options with a default sorting on server side
 	$scope.phones3 = {};
 	$scope.phones3.options = {
-		listURL: '/examples/db/get_phones.php',
+		listURL: 'http://flexilist.16mb.com/examples/db/get_phones.php',
 		method: 'GET',
 		urlencoded: true,
 		orderby: [
